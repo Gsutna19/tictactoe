6cb354e8661aface21ad0b2653d5ca7ee7feb147
Gsutna19 <fe.lipe19menezes@gmail.com>
2022-12-17 14:36:02 -0500

    Link page to css and js files.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
-const gameboard = (() => {
-    
-})
+const Gameboard = (() => {
+    let gameboard = [];
+
+    return {
+        gameboard
+    };
+})();
+
+const Player = (name) => {
+    let name = name;
+    return {name};
+}
+
+const Player2 = (name) => {
+
+    const {name} = Player(name);
+    let placeMark = "x";
+}
+// console.log("It's connected!")

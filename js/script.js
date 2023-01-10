8bd99237eb638ac9e4d05f8e92c0b1ab6e7b4d94
Gsutna19 <fe.lipe19menezes@gmail.com>
2023-01-10 10:04:55 -0500

    Create logic to select an individual square once

--- js/script.js
+++ js/script.js
@@ -1,19 +1,47 @@
+
 const Gameboard = (() => {
-    let gameboard = [];
+    // Is it ok to have gameboard be public??
+    const gameboard = [0, 1, 2, 3, 4, 5, 6, 7, 8];
+    const cellElements = document.querySelectorAll('[data-cell]');
+    // cellElements.forEach(cell => {
+    //     cell.addEventListener('click', handleClick, {once: true })
+    // })
 
-    return {
-        gameboard
-    };
+    // function handleClick(e) {
+    //     console.log('clicked')
+    // }
+
+    return {gameboard, cellElements};
 })();
+    
+const gameLogic = (() => {
+    const els = Gameboard.cellElements
+    els.forEach(cell => {
+        cell.addEventListener('click', handleClick, {once: true })
+    })
+    function handleClick(e) {
+        console.log('clicked')
+        const X_CLASS = 'x';
+        const CIRCLE_CLASS = 'circle';
+        let circleTurn
+        const cell = e.target
+        const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
 
-const Player = (name) => {
-    let name = name;
-    return {name};
-}
+        placeMark(cell, currentClass)
+        
+    }
+    function placeMark(cell, currentClass) {
+        cell.classList.add(currentClass)
+    }
+})();
+    
+const playerFactory = (name, mark) => {
+    return {name, mark};
+};
+    
 
-const Player2 = (name) => {
 
-    const {name} = Player(name);
-    let placeMark = "x";
-}
-// console.log("It's connected!")
+// const jeff = playerFactory('jeff', 'X');
+
+// console.log(jeff);
+console.log(Gameboard.gameboard, Gameboard.handleClick)
